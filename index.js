c7d253f3bd34640fdaea612cde8afc3b3632b8cd
Connor Mellencamp <connorsmac@Connors-MacBook-Pro.local>
2018-12-02 17:52:41 -0500

    yay

---
+++ index.js
@@ -0,0 +1,17 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on port 3000');
+});
